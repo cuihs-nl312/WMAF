74bffc481e3d418b9888c0b7d1aae26553d6cf7b
cuihs-nl312 <1440064417@qq.com>
2025-07-31 15:29:35 +0800

    Update extract_14_gene_sequences_for_alignment.py

--- extract_14_gene_sequences_for_alignment.py
+++ extract_14_gene_sequences_for_alignment.py
@@ -1,42 +1,34 @@
-cds_or_pep_file = input('enter the cds_or_pep_file : ')
-output = input('enter the core_genes_cds or core_genes_pep file : ')
-strain_ID = input('enter the strain ID : ')
+# 程序功能：根据菌株ID按顺序串联14个基因的序列
+
+gene_path_list_file = input("enter the 14 genes' path list file: ")
+strain_ID_list = input('enter the strain ID list file: ')
+output = input('enter the integrate_genes file: ')
 
 list_14_genes = [
-    'atp6',
-    'atp8',
-    'atp9',
-    'cob',
-    'cox1',
-    'cox2',
-    'cox3',
-    'nad1',
-    'nad2',
-    'nad3',
-    'nad4',
-    'nad4L',
-    'nad5',
-    'nad6',
+    'atp6', 'atp8', 'atp9', 'cob', 'cox1', 'cox2', 'cox3',
+    'nad1', 'nad2', 'nad3', 'nad4', 'nad4L', 'nad5', 'nad6',
 ]
 
-OB = open(cds_or_pep_file, 'r')
-OC = open(output, 'w')
-OC.write('>'+strain_ID+'\n')
-for i in range(0, len(list_14_genes), 1):
-    gene_ID = list_14_genes[i]
-    OB.seek(0)                               #程序的"灵魂"
-    OB_line = OB.readline()
-    while OB_line:
-        if '[' + 'gene' + '=' + gene_ID + ']' in OB_line:
-            #OC.write('>' + strain_ID + '|' + str(gene_ID) + '\n')
-            OB_line = OB.readline()
-            while '>' not in OB_line or OB_line != '':
-                OC.write(OB_line)
-                OB_line = OB.readline()
-                if '>' in OB_line or OB_line == '':
-                    break
-        OB_line = OB.readline()
-OB.close()
-OC.close()
-
-
+with open(strain_ID_list, 'r') as OA, open(output, 'w') as OD:
+    OA_line = OA.readlines()
+    for i in range(0, len(OA_line), 1):
+        ID_name = OA_line[i].split('\n')[0]
+        OD.write('>' + ID_name + '\n')
+        for j in range(0, len(list_14_genes), 1):
+            gene_ID = list_14_genes[j]
+            with open(gene_path_list_file, 'r') as OB:
+                OB.seek(0)
+                OB_line = OB.readlines()
+                for k in range(0, len(OB_line), 1):
+                    path = OB_line[k].split('\n')[0]
+                    with open(path, 'r') as OC:
+                        OC_line = OC.readlines()
+                        for m in range(0, len(OC_line), 1):
+                            if f'>{ID_name}|{gene_ID}' in OC_line[m]:
+                                for n in range(m+1, len(OC_line), 1):
+                                    if '>' not in OC_line[n]:
+                                        sequence = OC_line[n].split('\n')[0]
+                                        OD.write(sequence)
+                                    else:
+                                        break
+        OD.write('\n')
